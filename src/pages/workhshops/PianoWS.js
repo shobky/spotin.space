f60767ae19de570377e777ddfc47b2288631c07b
shobkeyy <shobkyy@gmail.com>
2022-11-06 09:30:23 +0200

    'workshops'

---
+++ src/pages/workhshops/PianoWS.js
@@ -0,0 +1,63 @@
+import React, { useState } from 'react'
+import piano from '../../assets/workhsops/piano.jpg'
+import { IoMdSwap } from 'react-icons/io'
+import { MdExpandMore } from 'react-icons/md'
+import { RiMenuAddLine } from 'react-icons/ri'
+import { IoClose } from 'react-icons/io5'
+
+
+const PianoWS = () => {
+    const [workshopOpen, setWorkshopOpen] = useState(false)
+
+
+    const openWorkShop = () => {
+        setWorkshopOpen(true)
+    }
+
+    const closeWorkshop = () => {
+        setWorkshopOpen(false)
+    }
+
+    return (
+        <div className='workshop-container'>
+            <div className='workshop_workshop__inactive'>
+                {
+                    workshopOpen ?
+                        <IoClose onClick={closeWorkshop} className="workshop_close-ico" />
+                        : ""
+                }
+                <img className='workshop-container_img' src={piano} alt="" />
+                <div id='workshopInfo' className='workshop-container_basic-info'>
+                    <div className='workshop_basic-info-header'>
+                        <p className='workshop_workshop-level'>Beginner to advanced</p>
+                        <p className='workshop_workshop-appointment'>Every Friday 1-3 PM</p>
+                    </div>
+                    <article className={workshopOpen ? 'workshop-container_article__active' : 'workshop-container_article'} id='workshopArticle'>
+                        <p className='workshop_article-head'>Each workshop is divided into pracitcal and theroratical parts {`( learan how to read notes )`}</p>
+                        <br />
+                        <p className='workshop_article-list-title'>Topics: </p>
+                        <ul className='workshop_article-topic-list'>
+                            <li>Piano Basics</li>
+                            <li>Techniques</li>
+                            <li>Musical notes</li>
+                            <li>Play your first musical piese</li>
+                        </ul>
+                        <br />
+                        <p>You'll need a Music notebook and a pencil. </p>
+                        <br />
+                        <p style={{ paddingBottom: "10px" }}>Phone number: 01271165428</p>
+                        <button className='workshop_join-btn-pc'><RiMenuAddLine /><span>FILL FORM</span></button>
+
+                    </article>
+                </div>
+                {
+                    workshopOpen ? "" :
+                        <button onClick={openWorkShop} className='workshop_expand-ico'><MdExpandMore /></button>
+                }
+
+            </div>
+        </div>
+    )
+}
+
+export default PianoWS
--- src/pages/workhshops/Workshop.js
+++ src/pages/workhshops/Workshop.js
@@ -1,6 +1,5 @@
 import React, { useState } from 'react'
 import './workshop.css'
-import piano from '../../assets/workhsops/piano.jpg'
 import english from '../../assets/workhsops/engllish.jpg'
 import graphic from '../../assets/workhsops/graphic.jpg'
 
@@ -14,6 +13,8 @@ import { Link } from 'react-router-dom'
 import { CgMenuRight } from 'react-icons/cg'
 import HomeMore from '../../components/nav/HomeMore'
 import { RiMenuAddLine } from 'react-icons/ri'
+import PianoWS from './PianoWS'
+import GraphicWS from './GraphicWS'
 
 const Workshop = () => {
     const page = "workshops"
@@ -26,57 +27,9 @@ const Workshop = () => {
     }
 
 
-    const openWorkshop = () => {
-        const workshopcontainer = document.getElementById('workshopContainer')
-        const workshopFlier = document.getElementById('workshopReadMore')
-        workshopFlier.classList.add("workshopReadMore__active")
 
-        workshopcontainer.classList.add('workshop_active')
-
-
-    }
-    const closeWorkshop = () => {
-        const workshopcontainer = document.getElementById('workshopContainer')
-        const workshopFlier = document.getElementById('workshopReadMore')
-        workshopFlier.classList.remove("workshopReadMore__active")
-        workshopcontainer.classList.remove('workshop_active')
-
-    }
-
-    const openWorkshopgd = () => {
-        const workshopcontainer = document.getElementById('workshopContainer')
-        const workshopFlier = document.getElementById('workshopReadMoregd')
-        workshopFlier.classList.add("workshopReadMore__active")
-
-        workshopcontainer.classList.add('workshop_active')
-
-    }
-    const closeWorkshopgd = () => {
-        const workshopcontainer = document.getElementById('workshopContainer')
-        const workshopFlier = document.getElementById('workshopReadMoregd')
-        workshopFlier.classList.remove("workshopReadMore__active")
-        workshopcontainer.classList.remove('workshop_active')
-
-    }
-
-    const openGraphic = () => {
-        const workshopcontainer = document.getElementById('workshopContainer')
-        const workshopFlier = document.getElementById('graphicReadMore')
-        workshopFlier.classList.add("workshopReadMore__active")
-
-        workshopcontainer.classList.add('workshop_active')
-
-    }
-    const closegraphic = () => {
-        const workshopcontainer = document.getElementById('workshopContainer')
-        const workshopFlier = document.getElementById('graphicReadMore')
-        workshopFlier.classList.remove("workshopReadMore__active")
-        workshopcontainer.classList.remove('workshop_active')
-
-    }
     return (
-        <div >
-
+        <div>
             <Nav className="workshops-nav" page={page} />
 
             <header className='workhsops-header'>
@@ -89,94 +42,15 @@ const Workshop = () => {
                 </div>
                 <HomeMore />
             </header>
-            <div id='workshopContainer' className='workshop'>
 
-                <div id='graphicReadMore' className='workshop_flier-container'>
-                    <img className='workhshop_flier-cover-photo' src={graphic} alt="" />
-                    <div className='workhshop_flier-cover-filter'>
-                        <IoClose onClick={closegraphic} className="workshop_close-ico" />
-                        <h1 className='workshop_workshop-name workshop_workshop-name__graphic'>Graphic Design Workshop</h1>
-                        <p className='workshop_workshop-level'>Beginner Level</p>
-                        <p className='workshop_workshop-appointment'>Every Mon & Wed 4-6 PM</p>
-                        <button className='workshop_join-btn'><RiMenuAddLine /></button>
-                        <button onClick={openGraphic} className='workshop_expand-ico'><MdExpandMore /></button>
-                        <article>
-                            <p className='workshop_article-head'>Each workshop is divided into pracitcal and theroratical parts {`( learan how to read notes )`}</p>
-                            <p className='workshop_article-list-title'>Topics: </p>
-                            <ul className='workshop_article-topic-list'>
-                                <li>understanding the program interface </li>
-                                <li>mastering layers with blending options</li>
-                                <li>detection skills and usefull shortcuts</li>
-                                <li>making a new design and how to make creative ideas</li>
-                            </ul>
-                            <br />
-                            <p>You'll need a labtop with average performance. </p>
-                            <br />
-                            <p style={{ paddingBottom: "10px" }}>Phone number: 01211216618</p>
-                            <button className='workshop_join-btn-pc'><RiMenuAddLine /></button>
+            <main className='pspojfdslk'>
+               <PianoWS/>
+               <GraphicWS/>
+               <PianoWS/>
+               <GraphicWS/>
+            </main>
 
-                        </article>
-                    </div>
-                </div>
-
-                <div id='workshopReadMore' className='workshop_flier-container'>
-                    <img className='workhshop_flier-cover-photo' src={piano} alt="" />
-                    <div className='workhshop_flier-cover-filter'>
-                        <IoClose onClick={closeWorkshop} className="workshop_close-ico" />
-                        <h1 className='workshop_workshop-name'>Piano Workshop</h1>
-                        <p className='workshop_workshop-level'>Beginner to advanced</p>
-                        <p className='workshop_workshop-appointment'>Every Friday 1-3 PM</p>
-                        <button className='workshop_join-btn'><RiMenuAddLine /></button>
-                        <button onClick={openWorkshop} className='workshop_expand-ico'><MdExpandMore /></button>
-                        <article>
-                            <p className='workshop_article-head'>Each workshop is divided into pracitcal and theroratical parts {`( learan how to read notes )`}</p>
-                            <p className='workshop_article-list-title'>Topics: </p>
-                            <ul className='workshop_article-topic-list'>
-                                <li>Piano Basics</li>
-                                <li>Techniques</li>
-                                <li>Musical notes</li>
-                                <li>Play your first musical piese</li>
-                            </ul>
-                            <br />
-                            <p>You'll need a Music notebook and a pencil. </p>
-                            <br />
-                            <p style={{ paddingBottom: "10px" }}>Phone number: 01271165428</p>
-                            <button className='workshop_join-btn-pc'><RiMenuAddLine /></button>
-
-                        </article>
-                    </div>
-                </div>
-
-                {/* <div id='workshopReadMoregd' className='workshop_flier-container'>
-                    <img className='workhshop_flier-cover-photo' src={english} alt="" />
-                    <div className='workhshop_flier-cover-filter'>
-                        <IoClose onClick={closeWorkshopgd} className="workshop_close-ico" />
-                        <h1 className='workshop_workshop-name'>English Workshop</h1>
-                        <p className='workshop_workshop-level'>Beginner to advanced</p>
-                        <p className='workshop_workshop-appointment'>Every Friday 1-3 PM</p>
-                        <p className='workshop_workshop-price'>Price: 250LE / Month.</p>
-                        <button className='workshop_join-btn'><RiMenuAddLine /></button>
-                        <button onClick={openWorkshopgd} className='workshop_expand-ico'><MdExpandMore /></button>
-                        <article>
-                            <p className='workshop_article-head'>Each workshop is divided into pracitcal and theroratical parts {`( learan how to read notes )`}</p>
-                            <p className='workshop_article-list-title'>Topics: </p>
-                            <ul className='workshop_article-topic-list'>
-                                <li>Piano Basics</li>
-                                <li>Techniques</li>
-                                <li>Musical notes</li>
-                                <li>Play your first musical piese</li>
-                            </ul>
-                            <br />
-                            <p>You'll need a Music notebook and a pencil. </p>
-                            <br />
-                            <p style={{ paddingBottom: "10px" }}>Phone number: 01271165428</p>
-                            <button className='workshop_join-btn-pc'><RiMenuAddLine /></button>
-
-                        </article>
-                    </div>
-                </div> */}
-
-            </div></div>
+        </div>
     )
 }
 
